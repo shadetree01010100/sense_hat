4065521c819ae79388c7ee8529d0be4086a97289
Tom Young <shadetreemetalworks@gmail.com>
2020-01-27 11:57:36 -0700

    read 9dof sensors

--- sense_hat_block.py
+++ sense_hat_block.py
@@ -1,12 +1,46 @@
+try:
+    from sense_hat import SenseHat
+except:
+    # catch exceptions so tests can run on alternate platforms
+    pass
 from nio import Block
-from nio.properties import VersionProperty
+from nio.block.mixins import EnrichSignals
+from nio.properties import BoolProperty, ObjectProperty, PropertyHolder, \
+    VersionProperty
 
 
-class SenseHat(Block):
+class IMUsensor(PropertyHolder):
+    accel = BoolProperty(title='Accelerometer', default=True, order=0)
+    compass = BoolProperty(title='Compass', default=True, order=1)
+    gyro = BoolProperty(title='Gyroscope', default=True, order=2)
+
+
+class SenseHat(Block, EnrichSignals):
+
+    imu = ObjectProperty(IMUsensor, title='IMU Sensor')
 
     version = VersionProperty('0.1.0')
 
+    def __init__(self):
+        super().__init__()
+        self.hat = None
+
+    def configure(self, context):
+        super().configure(context)
+        self.hat = SenseHat()
+        self.hat.set_imu_config(
+            self.imu().accel(),
+            self.imu().compass(),
+            self.imu().gyro())
+
     def process_signals(self, signals):
+        data = {}
+        if self.imu().accel():
+            data['accelerometer'] = self.hat.get_accelerometer_raw()
+        if self.imu().compass():
+            data['compass'] = self.hat.get_compass_raw()
+        if self.imu().gyro():
+            data['gyroscope'] = self.hat.get_gyroscope_raw()
         for signal in signals:
-            pass
+            signal = self.get_output_signal(data, signal)
         self.notify_signals(signals)
--- tests/test_sense_hat_block.py
+++ tests/test_sense_hat_block.py
@@ -1,16 +1,137 @@
+from unittest.mock import patch, Mock
 from nio import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
 from ..sense_hat_block import SenseHat
 
 
+@patch(SenseHat.__module__ + '.SenseHat')
 class TestSenseHat(NIOBlockTestCase):
 
-    def test_process_signals(self):
-        """Signals pass through block unmodified."""
+    def test_accelerometer(self, mockSenseHat):
+        """Signals are enriched with accelerometer data."""
+        mock_hat = Mock()
+        mock_hat.get_accelerometer_raw.return_value = {
+            'x': -1,
+            'y': 0,
+            'z': 1,
+        }
+        mockSenseHat.return_value = mock_hat
         blk = SenseHat()
-        self.configure_block(blk, {})
+        self.configure_block(blk, {
+            'imu': {
+                'accel': True,
+                'compass': False,
+                'gyro': False,
+            },
+            'enrich': {
+                'exclude_existing': False,
+            },
+        })
+
         blk.start()
-        blk.process_signals([Signal({'hello': 'nio'})])
+        mockSenseHat.assert_called_once_with()
+        mock_hat.set_imu_config.assert_called_once_with(True, False, False)
+
+        blk.process_signals([Signal({'pi': 3.14})])
+        mock_hat.get_accelerometer_raw.assert_called_once_with()
+        mock_hat.get_compass_raw.assert_not_called()
+        mock_hat.get_gyroscope_raw.assert_not_called()
+
         blk.stop()
         self.assert_num_signals_notified(1)
-        self.assert_last_signal_notified(Signal({'hello': 'nio'}))
+        self.assert_last_signal_list_notified([
+            Signal({
+                'accelerometer': {
+                    'x': -1,
+                    'y': 0,
+                    'z': 1,
+                },
+                'pi': 3.14,
+            }),
+        ])
+
+    def test_compass(self, mockSenseHat):
+        """Signals are enriched with compass data."""
+        mock_hat = Mock()
+        mock_hat.get_compass_raw.return_value = {
+            'x': -1,
+            'y': 0,
+            'z': 1,
+        }
+        mockSenseHat.return_value = mock_hat
+        blk = SenseHat()
+        self.configure_block(blk, {
+            'imu': {
+                'accel': False,
+                'compass': True,
+                'gyro': False,
+            },
+            'enrich': {
+                'exclude_existing': False,
+            },
+        })
+
+        blk.start()
+        mockSenseHat.assert_called_once_with()
+        mock_hat.set_imu_config.assert_called_once_with(False, True, False)
+
+        blk.process_signals([Signal({'pi': 3.14})])
+        mock_hat.get_accelerometer_raw.assert_not_called()
+        mock_hat.get_compass_raw.assert_called_once_with()
+        mock_hat.get_gyroscope_raw.assert_not_called()
+
+        blk.stop()
+        self.assert_num_signals_notified(1)
+        self.assert_last_signal_list_notified([
+            Signal({
+                'compass': {
+                    'x': -1,
+                    'y': 0,
+                    'z': 1,
+                },
+                'pi': 3.14,
+            }),
+        ])
+
+    def test_gyroscope(self, mockSenseHat):
+        """Signals are enriched with gyroscope data."""
+        mock_hat = Mock()
+        mock_hat.get_gyroscope_raw.return_value = {
+            'x': -1,
+            'y': 0,
+            'z': 1,
+        }
+        mockSenseHat.return_value = mock_hat
+        blk = SenseHat()
+        self.configure_block(blk, {
+            'imu': {
+                'accel': False,
+                'compass': False,
+                'gyro': True,
+            },
+            'enrich': {
+                'exclude_existing': False,
+            },
+        })
+
+        blk.start()
+        mockSenseHat.assert_called_once_with()
+        mock_hat.set_imu_config.assert_called_once_with(False, False, True)
+
+        blk.process_signals([Signal({'pi': 3.14})])
+        mock_hat.get_accelerometer_raw.assert_not_called()
+        mock_hat.get_compass_raw.assert_not_called()
+        mock_hat.get_gyroscope_raw.assert_called_once_with()
+
+        blk.stop()
+        self.assert_num_signals_notified(1)
+        self.assert_last_signal_list_notified([
+            Signal({
+                'gyroscope': {
+                    'x': -1,
+                    'y': 0,
+                    'z': 1,
+                },
+                'pi': 3.14,
+            }),
+        ])
